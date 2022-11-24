b0d1454919c144873b1fbd2191079f88f6cc356f
BmRahman <bmrahman23@gmail.com>
2022-11-25 00:08:46 +0600

    jwt done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -22,6 +23,20 @@ async function run() {
             const user = req.body;
             const result = await usersCollection.insertOne(user)
             res.send(result)
+        })
+
+       
+
+         // issue jwt token
+         app.get('/jwt', async(req, res) => {
+            const email = req.query.email;
+            const query = {email: email};
+            const user = await usersCollection.findOne(query)
+            if(user){
+                const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '24h'})
+                return res.send({accessToken: token})
+            }
+            res.status(401).send({accessToken: 'invalid user'})
         })
         
     }
