adec60fe2c55b07a603bdb57c17f4bf752d87b82
BmRahman <bmrahman23@gmail.com>
2022-11-27 15:16:16 +0600

    my orders api done

--- index.js
+++ index.js
@@ -136,6 +136,14 @@ async function run() {
             res.send(result)
         })
 
+        // delete a booking
+        app.delete('/bookings/:id', async(req, res) => {
+            const id = req.params.id;
+            const filter = {_id: ObjectId(id)}
+            const result = await bookingsCollection.deleteOne(filter)
+            res.send(result)
+        })
+
        
         
     }
